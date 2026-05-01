a3d6c310f549bb7c335069ba7ac7dab83ae6bf34
hellik-droid <hellik@amikompurwokerto.ac.id>
2026-05-01 15:58:49 +0700

    fix map typescript position type

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,23 +4,37 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function Map({ data }: any) {
+  const validData = data.filter(
+    (item: any) =>
+      item.latitude !== null &&
+      item.longitude !== null &&
+      item.latitude !== undefined &&
+      item.longitude !== undefined
+  );
+
+  const center: [number, number] = [-7.4246, 109.2396];
+
   return (
     <MapContainer
-      center={[-7.4246, 109.2396]}
+      center={center}
       zoom={13}
       style={{ height: "500px", width: "100%", marginTop: "20px" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {data.map((item: any, index: number) => (
-        <Marker key={index} position={[item.latitude, item.longitude]}>
-          <Popup>
-            {item.athlete_name}
-            <br />
-            {item.latitude}, {item.longitude}
-          </Popup>
-        </Marker>
-      ))}
+      {validData.map((item: any, index: number) => {
+        const position: [number, number] = [item.latitude, item.longitude];
+
+        return (
+          <Marker key={index} position={position}>
+            <Popup>
+              {item.athlete_name}
+              <br />
+              {item.latitude}, {item.longitude}
+            </Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
   );
 }
